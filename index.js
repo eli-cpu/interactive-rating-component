7e280645060e6a888ae76a00bd90673db0cb6465
Elias Anwari <eli-cpu>
2025-02-24 19:15:53 +0100

    added javascript stuff

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let rated = false;
+let rating = 0;
+
+
+function submit() {
+    if(rated) {
+        document.getElementById("submit").style.backgroundColor = "white";
+        document.getElementById("submit").style.color = "black";
+        window.location.replace("submitted-state.html");
+    }
+
+}
+
+function rate(id) {
+    if(rated) {
+        for (let i = 1; i <= 5; i++) {
+            let button = document.getElementById(i.toString());
+            button.style.backgroundColor = "hsl(218, 18%, 19%)";
+            button.style.color = "hsl(217, 12%, 63%)";
+        }
+    }
+    document.getElementById(id).style.backgroundColor = "white";
+    document.getElementById(id).style.color = "black";
+    rated = true;
+    rating = id;
+}
+
