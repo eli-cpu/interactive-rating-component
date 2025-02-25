09b7b48caa6141caa0feac4dfe09bba4c682f5b6
Elias Anwari <eli-cpu>
2025-02-25 20:48:11 +0100

    testing

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ function submit() {
     if(rated) {
         document.getElementById("submit").style.backgroundColor = "white";
         document.getElementById("submit").style.color = "black";
-        console.log("hello there");
         window.location.replace("submitted-state.html");
-        console.log("hello there 2");
-        console.log(rating);
-        document.getElementById("selected").innerText = "You selected " + rating + " out of 5";
+        document.getElementById("selected").innerHTML = "You selected " + rating + " out of 5";
     }
 
 }
@@ -19,6 +16,7 @@ function rate(id ) {
     if(rated) {
         for (let i = 1; i <= 5; i++) {
             let button = document.getElementById(i.toString());
+            button.style
             button.style.backgroundColor = "hsl(218, 18%, 19%)";
             button.style.color = "hsl(217, 12%, 63%)";
         }
