9086acc7ee21cb55be6a9733ccbc7b9f751713e1
Elia$ <63410316+eli-cpu@users.noreply.github.com>
2025-02-25 15:25:04 +0100

    changed some javascript stuff

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 let rated = false;
 let rating = 0;
 
-
 function submit() {
     if(rated) {
         document.getElementById("submit").style.backgroundColor = "white";
         document.getElementById("submit").style.color = "black";
+        console.log("hello there");
         window.location.replace("submitted-state.html");
-        let s = "You selected " + rating.toString() + " out of 5";
-        document.getElementById("selected-text").innerText = s;
+        console.log("hello there 2");
+        console.log(rating);
+        document.getElementById("selected").innerText = "You selected " + rating + " out of 5";
     }
 
 }
 
-function rate(id) {
+function rate(id ) {
     if(rated) {
         for (let i = 1; i <= 5; i++) {
             let button = document.getElementById(i.toString());
@@ -22,8 +23,8 @@ function rate(id) {
             button.style.color = "hsl(217, 12%, 63%)";
         }
     }
-    document.getElementById(id).style.backgroundColor = "white";
-    document.getElementById(id).style.color = "black";
+    document.getElementById(id.toString()).style.backgroundColor = "white";
+    document.getElementById(id.toString()).style.color = "black";
     rated = true;
     rating = id;
 }
